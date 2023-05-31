c82cb8252ebe09401a62f891878133967c3ad203
Ashwini <ashwiniparaye9309@gmail.com>
2023-05-31 18:56:31 +0530

    refresh token created

--- server.js
+++ server.js
@@ -21,6 +21,48 @@ const users = [
   },
 ];
 
+let refreshTokens = [];
+
+// refresh and regenerate access token
+app.post("/api/refresh", (req, res) => {
+  //take the refresh token from user
+  const refreshToken = req.body.token;
+
+  //send error ig token is not valid
+  if (!refreshToken) return res.status(401).json("You are not authenticated!");
+
+  if (!refreshTokens.includes(refreshToken)) {
+    return res.status(403).json("Refresh token is not valid!");
+  }
+  jwt.verify(refreshToken, "refreshSecretKey", (err, user) => {
+    err && console.log(err);
+    refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+
+    const newAccessToken = generateAccessToken(user);
+    const newRefreshToken = generateAccessToken(user);
+
+    refreshTokens.push(newRefreshToken);
+    res
+      .status(200)
+      .json({ accessToken: newAccessToken, refreshToken: newRefreshToken });
+  });
+
+  //if evereything ok, create new access token, refresh token and send to user
+});
+
+const generateAccessToken = (user) => {
+  return jwt.sign(
+    { id: user.id, isAdmin: user.isAdmin },
+    "secretKey",
+    //token expiration time
+    { expiresIn: "30s" }
+  );
+};
+const generateRefreshToken = (user) => {
+  return jwt.sign({ id: user.id, isAdmin: user.isAdmin }, "refreshSecretKey");
+};
+
+//login api
 app.post("/api/login", (req, res) => {
   const { username, password } = req.body;
   const user = users.find((u) => {
@@ -28,14 +70,15 @@ app.post("/api/login", (req, res) => {
   });
   if (user) {
     //generate access token
-    const accessToken = jwt.sign(
-      { id: user.id, isAdmin: user.isAdmin },
-      "secretKey"
-    );
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
+    refreshTokens.push(refreshToken);
+
     res.json({
       username: user.username,
       isAdmin: user.isAdmin,
       accessToken,
+      refreshToken,
     });
   } else {
     res.status(400).json("Username or Password incorrect!");
